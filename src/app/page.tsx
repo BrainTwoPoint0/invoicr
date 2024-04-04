8e4472d941375daf8a49b8a6d28d3cf04f4a1edf
Karim Fawaz <karimhilalfawaz@gmail.com>
2024-04-05 00:24:35 +0100

    remove local storage and change styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,31 +7,32 @@ import InvoicePage from "./components/InvoicePage";
 import { Invoice } from "./data/types";
 
 export default function Home() {
-  let data = null;
-  if (typeof window !== "undefined") {
-    try {
-      const savedInvoice = window.localStorage.getItem('invoiceData');
-      if (savedInvoice) {
-        data = JSON.parse(savedInvoice);
-      }
-    } catch (error) {
-      console.error("Failed to parse saved invoice data:", error);
-    }
-  }
+  // let data = null;
+  // if (typeof window !== "undefined") {
+  //   try {
+  //     const savedInvoice = window.localStorage.getItem('invoiceData');
+  //     if (savedInvoice) {
+  //       data = JSON.parse(savedInvoice);
+  //     }
+  //   } catch (error) {
+  //     console.error("Failed to parse saved invoice data:", error);
+  //   }
+  // }
 
-  const onInvoiceUpdated = (invoice: Invoice) => {
-    if (typeof window !== "undefined") {
-      try {
-        window.localStorage.setItem('invoiceData', JSON.stringify(invoice));
-      } catch (error) {
-        console.error("Failed to save invoice data:", error);
-      }
-    }
-  };
+  // const onInvoiceUpdated = (invoice: Invoice) => {
+  //   if (typeof window !== "undefined") {
+  //     try {
+  //       window.localStorage.setItem('invoiceData', JSON.stringify(invoice));
+  //     } catch (error) {
+  //       console.error("Failed to save invoice data:", error);
+  //     }
+  //   }
+  // };
   return (
     <main className="mt-8 mb-12 mx-auto w-[700px]">
       <h1 className="text-3xl text-center font-bold mb-20">HAPPY BIRTHDAY MOM</h1>
-      <InvoicePage data={data} onChange={onInvoiceUpdated} />
+      <InvoicePage />
+      {/* <InvoicePage data={data} onChange={onInvoiceUpdated} /> */}
     </main>
   );
 }
